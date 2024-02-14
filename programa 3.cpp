956c4d11de2d12648987822ba76afcc427d7ce64
Raimi-dev <65572276+Raimi-dev@users.noreply.github.com>
2024-02-14 15:54:20 -0300

    Add files via upload
    Um dos primeiros códigos que criei no espectro da linguagem C(C - C++).

---
+++ programa 3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int numero;
+	numero=2;
+	char letra;
+	letra='b';
+	float flutuante;
+	flutuante=3.15;
+	printf("O valor da variavel e %d",numero);
+	printf("\n\n");
+	printf("O valor da variavel e %c",letra);
+	printf("\n\n");
+	printf("O valor da variavel e %f",flutuante);
+	printf("\n\n");
+	system("PAUSE");
+	return 0;
+	
+}
