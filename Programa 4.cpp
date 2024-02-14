fc25a27aad491bf4298a5f1f815bcb2ddee11937
Raimi-dev <65572276+Raimi-dev@users.noreply.github.com>
2024-02-14 15:57:58 -0300

    Add files via upload
    Um dos primeiros códigos que criei no espectro da linguagem C(C - C++).

---
+++ Programa 4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	/*int numero1;
+	numero1=3;
+	int numero2;
+	numero2=15;*/
+	int numero1=1;
+	int numero2=2;
+	printf("O valor da variavel numero1 e %d e o valor da variavel numero2 e %d",numero1,numero2);
+	system("PAUSE");
+	return 0;
+}
