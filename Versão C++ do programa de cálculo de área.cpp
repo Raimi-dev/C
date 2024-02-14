74bfd3ba57046665eecac0f3e093a0033c5c0f97
Raimi-dev <65572276+Raimi-dev@users.noreply.github.com>
2024-02-14 16:52:57 -0300

    Update and rename Versão C++.cpp to Versão C++ do programa de cálculo de área.cpp

--- Versão C++ do programa de cálculo de área.cpp
+++ Versão C++ do programa de cálculo de área.cpp
@@ -4,13 +4,13 @@ int area(int base,int altura);
 
 int main() {
 	int a,b,ret;
-	printf("\nDigite o valor da base do ret‚ngulo:""");
+	printf("\nDigite o valor da base do ret√¢ngulo:""");
 	scanf("%d", &b);
-	printf("\nDigite o valor da altura do ret‚qngulo: ");
+	printf("\nDigite o valor da altura do ret√¢qngulo: ");
 	scanf("%d", &a);
 	
 	ret = area (b,a);
-	printf("\nArea do ret‚nguloo = %d", ret);
+	printf("\nArea do ret√¢nguloo = %d", ret);
 	system("PAUSE");
 }
 
